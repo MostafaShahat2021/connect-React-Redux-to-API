6b2afe4ae8eed0aa4033ea53491001e6d47c8827
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2023-08-23 17:01:14 +0300

    Update initialState

--- src/redux/users/usersSlice.js
+++ src/redux/users/usersSlice.js
@@ -2,12 +2,17 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   users: [],
+  isLoading: false,
+  error: undefined,
 };
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {
+
+  },
+  extraReducers: {
 
   },
 })
